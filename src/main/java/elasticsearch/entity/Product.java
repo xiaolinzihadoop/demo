ed0298b92a1823bb15dac8205b94cd3748cf79f4
xiaolinzihadoop <java_2011android@163.com>
2023-04-01 09:31:27 +0800

    324

--- src/main/java/elasticsearch/entity/Product.java
+++ src/main/java/elasticsearch/entity/Product.java
@@ -9,8 +9,8 @@ import org.springframework.data.elasticsearch.annotations.FieldType;
 import java.math.BigDecimal;
 
 /**
- * @author WH
- * @version 1.0
+ * @author WH xiaolinzi
+ * @version 1.0 
  * @date 2020/6/17 23:03
  * @Description TODO
  */
