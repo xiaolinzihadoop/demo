a762f1185df5462252690d5c3199362b3b46e3b4
xiaolinzihadoop <java_2011android@163.com>
2023-04-01 09:20:59 +0800

    32434

--- src/main/java/elasticsearch/Dao/ProductMapper.java
+++ src/main/java/elasticsearch/Dao/ProductMapper.java
@@ -10,7 +10,7 @@ import java.math.BigDecimal;
 import java.util.List;
 
 /**
- * @author WH xiaolinzi
+ * @author WH xiaolinzi234324
  * @version 1.0
  * @date 2020/6/17 23:16
  * @Description 继承 ElasticsearchRepository 获取基本的es操作
