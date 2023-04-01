1468f2b36eae9761c241eeb985390ee23a380266
xiaolinzihadoop <java_2011android@163.com>
2023-04-01 09:15:07 +0800

    32324

--- src/main/java/elasticsearch/Dao/ProductMapper.java
+++ src/main/java/elasticsearch/Dao/ProductMapper.java
@@ -16,44 +16,45 @@ import java.util.List;
  * @Description 继承 ElasticsearchRepository 获取基本的es操作
  */
 public interface ProductMapper extends ElasticsearchRepository<Product, Long> {
-    /**
-     * 通过名字查询
-     * @param name
-     * @return
-     */
-    // Streamable<Product> findByName(String name);
-    List<Product> findByName(String  name);
+	/**
+	 * 通过名字查询
+	 * 
+	 * @param name
+	 * @return
+	 */
+	// Streamable<Product> findByName(String name);
+	List<Product> findByName(String name);
 
-    List<Product> findByDescribe(String describe);
+	List<Product> findByDescribe(String describe);
 
-    /**
-     * 通过名字分页查询
-     * @param name
-     * @param pageable
-     * @return
-     */
-    Page<Product> findByName(String name, Pageable pageable);
+	/**
+	 * 通过名字分页查询
+	 * 
+	 * @param name
+	 * @param pageable
+	 * @return
+	 */
+	Page<Product> findByName(String name, Pageable pageable);
 
-    /**
-     * 通过名字排序查询
-     * @param name
-     * @param sort
-     * @return
-     */
-    List<Product> findByName(String name, Sort sort);
+	/**
+	 * 通过名字排序查询
+	 * 
+	 * @param name
+	 * @param sort
+	 * @return
+	 */
+	List<Product> findByName(String name, Sort sort);
 
+	/**
+	 * 通过价格排序查询第一条
+	 * 
+	 * @param price
+	 * @return
+	 */
+	Product findFirstByPrice(BigDecimal price);
 
+	Product findTopByPrice(BigDecimal price);
 
-    /**
-     * 通过价格排序查询第一条
-     * @param price
-     * @return
-     */
-    Product findFirstByPrice(BigDecimal price);
-
-    Product findTopByPrice(BigDecimal price);
-
-    Page<Product> queryTop10ByName(String name, Pageable pageable);
-
+	Page<Product> queryTop10ByName(String name, Pageable pageable);
 
 }
